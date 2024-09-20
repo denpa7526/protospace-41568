08bcd54b55cfadfc4a6516aaf8768e69f602a780
denpa7526 <d19.sax8kww@gmail.com>
2024-09-20 17:13:03 +0900

    修正依頼③

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,10 +3,10 @@ class CommentsController < ApplicationController
   def create
     @prototype = Prototype.find(params[:prototype_id])
     @comment = @prototype.comments.build(comment_params)
-    @comments = @prototype.comments.includes(:user)
     if @comment.save
       redirect_to prototype_path(@prototype)
     else
+      @comments = @prototype.comments.includes(:user)
       render 'prototypes/show', status: :unprocessable_entity
     end
   end
@@ -16,5 +16,4 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:content).merge(user_id: current_user.id)
   end
-  
 end
