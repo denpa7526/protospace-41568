e0a85c155d29770e0a1d88c389e673d8be205199
denpa7526 <d19.sax8kww@gmail.com>
2024-09-20 17:20:14 +0900

    修正依頼④

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 
   def create
     @prototype = Prototype.find(params[:prototype_id])
-    @comment = @prototype.comments.build(comment_params)
+    @comment = Comment.new(comment_params)
     if @comment.save
       redirect_to prototype_path(@prototype)
     else
@@ -14,6 +14,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content).merge(user_id: current_user.id)
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
+
 end
